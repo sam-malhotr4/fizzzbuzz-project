26c1cee3ab6f5c2705af653369f0b336c18a01ae
saksham-s1ngh <saksham249singh@gmail.com>
2024-02-28 19:57:37 -0500

    fixed "fizzBuzz" method to pass the third test case (multiples of 5)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,8 @@ public class App
     public static String fizzBuzz(int number){
         if(number % 3 == 0){
             return "Fizz";
+        } else if(number % 5 == 0){
+            return "Buzz";
         }
         return String.valueOf(number);
     }
