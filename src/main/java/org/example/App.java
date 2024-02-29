a7ffa959dbf85369a4f428acc073dbfbc11a038a
saksham-s1ngh <saksham249singh@gmail.com>
2024-02-28 20:07:00 -0500

    updated the main method, loop through 1-100 to check fizzBuzz method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(fizzBuzz(i) + ", ");
+        }
     }
 
     public static String fizzBuzz(int number){
